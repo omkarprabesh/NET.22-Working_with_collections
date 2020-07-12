f1d19bd1caf263ed895ffdac46519d381511064c
Omkar Kandel Prabesh <omkar.prabesh@gmail.com>
2020-07-12 17:24:34 -0400

    In class assignment

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+namespace ClassQueue
+{
+    public class QCustom<T> : Queue
+    {
+
+        private int _position;
+
+        public void Add(T element)
+        {
+            if (_position == 10)
+            {
+                Remove();
+            }
+            Enqueue(element);
+            _position++;
+
+        }
+
+        public void Remove()
+        {
+            Dequeue();
+            _position--;
+            Console.WriteLine($"{Dequeue()} has been removed from the queue");
+        }
+    }
+        public class Execute
+        {
+            static void Main()
+            {
+                QCustom<string> CheckList = new QCustom<string>();
+                CheckList.Add("One");
+                CheckList.Add("One");
+                CheckList.Add("One");
+                CheckList.Add("One");
+                CheckList.Add("One");
+                CheckList.Add("One");
+                CheckList.Add("One");
+                CheckList.Add("One");
+                CheckList.Add("One");
+                CheckList.Add("One");
+                CheckList.Add("Two");
+                CheckList.Remove();
+            foreach (var x in CheckList)
+                    Console.WriteLine(x);
+            }
+        }
+    }
+
+
